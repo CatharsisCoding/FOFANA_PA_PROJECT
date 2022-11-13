3a6e493483bdf02a2fcbaf6954f62c43253a180c
abdel-malik fofana <97361977+catharsis33@users.noreply.github.com>
2022-11-13 01:19:37 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -17,35 +17,47 @@ music_pause=0
 
 
 #screen option
-ecran = pygame.display.set_mode((1366, 768))
+screen = pygame.display.set_mode((1366, 768))
 pygame.display.set_caption("Maliki-fencing®")
 
-#load background from file
+#load background
 bg_name="img/c.jpg"
+
+#load scene frop .ffscene file
+def load_scene(namefile):
+    with open(namefile) as my_file:
+        scene=my_file.read()
+    return scene
+current_scene=load_scene("ffscene/default.ffscene")
+print(current_scene)
 
 #fonction for drawing background on screen
 def display_bg(bg_name):
-    ecran.blit(pygame.image.load(bg_name).convert_alpha(),(0,0))
+    screen.blit(pygame.image.load(bg_name).convert_alpha(),(0,0))
 
 #option writing something in the screen
 police =pygame.font.SysFont("Arialblack",40) #font
+scene_police =pygame.font.SysFont("Arialblack",220) #font
 color_text=(0,0,0) #black color
 def texte(texte,police,couleur,x,y):
     img=police.render(texte,True,couleur)
-    ecran.blit(img,(x,y))
+    screen.blit(img,(x,y))
 
 continuer = True
 while continuer:
 
     #bg display
     display_bg(bg_name)
+    #scene display
+    current_scene.rstrip(current_scene[-1]) #corrige un bug
+    texte(current_scene,scene_police ,(0,0,255),0,600)
     #text display
     texte('appuyer sur "m" pour éteindre la musique',police ,color_text,0,25)
     texte("appuyer sur echap pour quitter",police ,color_text,0,0)
     #fps display
     clock.tick(fps)
     texte(str(round(clock.get_fps(),2)),police,(255, 255, 0),1300,0)
-
+    
     for event in pygame.event.get():
         #if we touch a key we do something:
         if event.type == pygame.KEYDOWN:
@@ -53,6 +65,7 @@ while continuer:
                 continuer = False
             if event.key == pygame.K_m:
                 bg_name="img/b.jpg"
+                current_scene=load_scene("ffscene/scene2.ffscene")
                 if music_pause==0:
                     pygame.mixer.music.pause()
                     music_pause=1
