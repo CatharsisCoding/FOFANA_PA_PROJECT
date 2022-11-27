3664dc6e54634eea5141d3449d74fe48dda5d939
abdel-malik fofana <97361977+catharsis33@users.noreply.github.com>
2022-11-27 19:55:39 +0100

    Add files via upload

--- a.py
+++ a.py
@@ -3,7 +3,9 @@ from random import randint
 import pygame
 import sys
 import time
+import threading
 from threading  import Timer
+from pynput.keyboard import *
 
 pygame.init()
 
@@ -20,12 +22,12 @@ fps = int(sys.argv[1])
 
 #constants
 
-WINDOW_WIDTH = 60 
-WINDOW_HEIGHT = 20 
+WIDTH = 60 
+HEIGHT = 20 
 
 # setup window
 curses.initscr()
-win = curses.newwin(WINDOW_HEIGHT, WINDOW_WIDTH, 0, 0) # rows, columns
+win = curses.newwin(HEIGHT, WIDTH, 0, 0) # rows, columns
 win.keypad(1) #easier to process keypad on terminal
 curses.noecho() #instant key process
 curses.curs_set(0) #hide the "cursor" in terminal
@@ -41,52 +43,191 @@ def load_scene(namefile):
     list1.pop()
     scene=""
     return scene.join(list1)
+
 current_scene=load_scene("ffscene/default.ffscene")
+current_scene = current_scene.replace("_"," ")
+
+
+
 
 # game logic
 score = [0,0]
-print(score[1])
 ESC = 27 #ascii
 key=curses.has_key(97)
-weapon_p1="/"
-weapon_p2="\\"
-x1=0
-y1=0
-x2=0
-y2=0
+weapon_p1=" "
+weapon_p2=" "
+rest_p1=" \\"
+rest_p2="/ "
+player1_attack=0
+player2_attack=0
+player1_block=0
+player2_block=0
+attack_speed= float(sys.argv[2])
+
+
+
+#x and y position of player 1
+positionx_p1=1
+positiony_p1=0
+#x and y position of player 2
+positionx_p2=53
+positiony_p2=0
+
+#print player 1 on screen
 def player1():
     #player1 display
-    win.addstr(14+y1, 1+x1, "<o>") 
-    win.addstr(15+y1, 2+x1, "|_"+ weapon_p1) 
-    win.addstr(16+y1, 2+x1, "|") 
-    win.addstr(17+y1, 2+x1, "|") 
-    win.addstr(18+y1, 1+x1, "/|")
+    win.addstr(14+positiony_p1, positionx_p1, "<o>") 
+    win.addstr(15+positiony_p1, positionx_p1, " |_"+ weapon_p1) 
+    win.addstr(16+positiony_p1, positionx_p1, " |"+rest_p1) 
+    win.addstr(17+positiony_p1, positionx_p1, " |") 
+    win.addstr(18+positiony_p1, positionx_p1, "/|")
+
+#print player 2 on screen
 def player2():
     #player2 display
-    win.addstr(14+y2, 54+x2, "<o>") 
-    win.addstr(15+y2, 53+x2, weapon_p2+"_|") 
-    win.addstr(16+y2, 55+x2, "|") 
-    win.addstr(17+y2, 55+x2, "|") 
-    win.addstr(18+y2, 55+x2, '|\\') 
+    win.addstr(14+positiony_p2, positionx_p2, " <o>") 
+    win.addstr(15+positiony_p2, positionx_p2, weapon_p2+"_|") 
+    win.addstr(16+positiony_p2, positionx_p2,"  |") , win.addstr(16+positiony_p2, positionx_p2, rest_p2) 
+    win.addstr(17+positiony_p2, positionx_p2, "  |") 
+    win.addstr(18+positiony_p2, positionx_p2+2, '|\\') 
+
+#clean the screen
 def clean():
-    for i in range(12,19):
+    for i in range(1,19):
         for j in range(1,59):
             win.addstr(i,j," ")
-def jump1_left():
-   global y1
-   y1+=1
-def jump1_right():
-   global y1 
-   y1+=1
+
+def jump1():
+   global positiony_p1 
+   positiony_p1+=1
 def jump2():
-   global y2
-   y2+=1
+   global positiony_p2
+   positiony_p2+=1
+def switch_weapon_p1():
+   global rest_p1
+   global weapon_p1
+   global player1_attack
+   global player1_block
+   player1_attack=0
+   player1_block=0
+   rest_p1=" \\"
+   weapon_p1=" "
+def switch_weapon_p2():
+   global rest_p2 
+   global weapon_p2
+   global player2_attack
+   global player2_block
+   player2_attack=0
+   player2_block=0
+   rest_p2="/ "
+   weapon_p2=" "
+
+
+
+#keyboard logic and thread (to simultaneously press key):
+
+#when we press key we do what is in this fonction (actually nothing)
+def press_on(key):
+    pass
+#when we unpress the key we do what is in this fonction
+def press_off(key):  
+    global positionx_p1
+    global positiony_p1
+    global rest_p1
+    global weapon_p1
+    global player1_attack
+    global player1_block
+
+    global positiony_p2
+    global positionx_p2 
+    global rest_p2
+    global weapon_p2
+    global player2_attack
+    global player2_block
+    try:
+        #player 1 keys set
+        if key.char == 'd':
+            if(positionx_p1<54):
+                positionx_p1+=1
+        if key.char == 'a': #press a jump left (ascii 97)
+            if(positionx_p1>1 and positiony_p1 >-2): # if we arrive at the left border we cannot jump left
+                positiony_p1-=1
+                positionx_p1-=2
+                t=Timer(0.25,jump1)
+                t.start()
+        if key.char == 'q': #press q go left
+            if(positionx_p1>1): #if we arrive at the left border we cannot go left
+                positionx_p1-=1
+        if key.char == 'e': #press e jump righ
+            if(positionx_p1<54 and positiony_p1 >-2):
+                positiony_p1-=1
+                positionx_p1+=2
+                t=Timer(0.25,jump1)
+                t.start()
+        if key.char == 'z': # press z to attack
+            rest_p1=""
+            weapon_p1="_"
+            player1_attack=1
+            t=Timer(attack_speed,switch_weapon_p1)
+            t.start()
+        if key.char == 's': # press s to block
+            rest_p1=""
+            weapon_p1="|"
+            t=Timer(0.5,switch_weapon_p1)
+            t.start()
+
+        #player 2 keys set
+        if key.char == 'i': # i 105 jump left
+            if(positionx_p2>2 and positiony_p2 >-2):
+                positiony_p2-=1
+                positionx_p2-=2
+                t=Timer(0.25,jump2)
+                t.start()
+        if key.char == 'm': #m 109 jump right
+            if(positionx_p2<54 and positiony_p2 >-2):
+                positiony_p2-=1
+                positionx_p2+=2
+                t=Timer(0.25,jump2)
+                t.start()
+        if key.char == 'p': #p 112 block
+            rest_p2=""
+            weapon_p2="|"
+            player2_block=1
+            t=Timer(2,switch_weapon_p2)
+            t.start()
+        if key.char == 'o': #o to attack
+            rest_p2=""
+            weapon_p2="_"
+            player2_attack=1
+            t=Timer(attack_speed,switch_weapon_p2)
+            t.start()
+            
+    except AttributeError:
+        #some player 2 keys set
+        if(key == Key.esc):
+            return False
+        if(key == Key.left):
+            if(positionx_p2>1):
+                positionx_p2-=1
+        if(key == Key.right):
+            if(positionx_p2<55):
+                positionx_p2+=1
+#key listener
+def keyboard():
+    with Listener(on_press=press_on ,on_release=press_off) as listener:
+        listener.join()
+
+#keyboard thread to simultaniously press key (player 1 can press key when player 2 press key too)
+keyboard_thread=threading.Thread(target=keyboard)
+keyboard_thread.start()
+
+# GAME LOOP
 
 while key != ESC:
 
     #display player
     clean()
-
+    win.addstr(18, 1, current_scene) #ffscene display
     player1()
     player2()
     #fps display
@@ -94,52 +235,23 @@ while key != ESC:
     win.addstr(0, 45, "FPS: " +str(round(clock.get_fps(),2)))
 
     win.addstr(1, 22, "Score : "+str(score[0])+" | "+str(score[1])) #score display
-    #win.addstr(19, 1, current_scene) #ffscene display
+    
 
 
-
-   
-
-   
     key = win.getch()  #we wait for the next charactere
-    #a 97 , q 113 , d 100 , i 105 , e 101 , m 109 , z 122,o 111
-    #movement player 1
-    if key == 97: #when we press a we jump (ascii 97)
-        y1-=1
-        x1-=2
-        t=Timer(0.25,jump1_left)
-        t.start()
-    if key == 113: #press q go left
-        x1-=1
-    if key == 100: #press d go right
-        x1+=1
-    if key == 101:
-        y1+=1
-        x1+=2
-        t=Timer(0.25,jump1_right)
-        t.start()
-    if key == curses.KEY_RIGHT:
-        pass    
-    #movement player 2
-    if key == curses.KEY_DOWN: 
-        y2+=1
-    if key == curses.KEY_UP:
-        y1-=1
-        t=Timer(0.25,jump2)
-        t.start()
-    if key == curses.KEY_LEFT:
-        pass
-    if key == curses.KEY_RIGHT:
-        pass    
 
-
-    # check if we hit the border
-   # if y == 0: break
-    #if y == WINDOW_HEIGHT-1: break
-    #if x == 0: break
-    #if x == WINDOW_WIDTH -1: break
-
-
+    if((positionx_p2 - positionx_p1) == 4):
+        if(player1_attack==1 and player2_block==0):
+            positionx_p1=1
+            positionx_p2=54
+            score[0]+=1
+            time.sleep(0.25)
+        if(player2_attack==1 and player1_block==0):
+            positionx_p1=1
+            positionx_p2=54
+            score[1]+=1
+            time.sleep(0.25)
+    
 
 curses.endwin()
 print(f"Final score = {score}")
